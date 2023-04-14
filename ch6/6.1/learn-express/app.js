4a3e5c5c34604fc67cef15226606f353bb58649e
최기훈 <86941191+choiKi@users.noreply.github.com>
2023-04-14 21:52:15 +0900

    0414

--- ch6/6.1/learn-express/app.js
+++ ch6/6.1/learn-express/app.js
@@ -1,12 +1,33 @@
+const cookieParser = require('cookie-parser');
 const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req, res) => {
-  // res.send('Hello, Express');
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.use((req, res, next) => {
+  console.log('모든 요청에서 실행됩니다');
+  next();
+});
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+app.get('/', (req, res, next) => {
+  console.log('GET / 요청에서만 실행됩니다.');
+  next(new Error('에러는 에러 처리 미들웨어로 갑니다.'));
+}, (req, res) => {
+  res.send('Hello, Express');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err);
 });
 
 app.listen(app.get('port'), () => {
